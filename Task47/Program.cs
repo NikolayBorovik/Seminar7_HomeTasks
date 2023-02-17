e5f804817d738a9e403e7dade7e08268db519507
Nikolay Borovik <nik.Borovik@bk.ru>
2023-02-17 16:03:12 +1000

    Task47 done

--- Task47/Program.cs
+++ Task47/Program.cs
@@ -1,2 +1,53 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+Console.Clear();
+
+int GetNum(string numberName)
+{
+    System.Console.WriteLine($"Enter {numberName}");
+    int num = Convert.ToInt32(Console.ReadLine());
+    return num;
+}
+
+double[,] GetMatrixDouble(int rows, int columns)
+{
+    double[,] matrix = new double[rows, columns];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            var random = new Random();
+            matrix[i, j] = random.Next(1,10) + random.NextDouble();
+            matrix[i, j] = Math.Round(matrix[i, j], 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrixDouble(double[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            System.Console.Write(matrix[i, j] + " ");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+int rows = GetNum("a number of rows");
+int columns = GetNum("a number of columns");
+
+double[,] matrix = GetMatrixDouble(rows, columns);
+
+PrintMatrixDouble(matrix);
+
+
+
+
+
